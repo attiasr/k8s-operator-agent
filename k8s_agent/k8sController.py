0f52ebebeeff3a02d4485b3a66f692c0384ceaac
Rudy Attias <rudy@mlxlabs.io>
2024-01-25 13:31:38 -0500

    Add CRD retrieval to k8sController.py and update root endpoint in webapp.py

--- k8s_agent/k8sController.py
+++ k8s_agent/k8sController.py
@@ -1,10 +1,11 @@
 from typing import Iterable, Mapping
 import asyncio
 import kopf
+import pykube
 from . import settings
 
 INDEX_MAP: Mapping[str, kopf.Index] = {}
-
+CRDS: Iterable[pykube.CustomResourceDefinition]
 
 def _update_index_map(**kwargs):
   global INDEX_MAP
@@ -21,22 +22,33 @@ def get_index(index_name: str) -> kopf.Index:
 @kopf.on.startup()
 async def configure(settings: kopf.OperatorSettings, logger, **_):
   logger.info("configuring k8sController")
+  global CRDS
+  api = pykube.HTTPClient(pykube.KubeConfig.from_env())
+  CRDS = [ crd.name for crd in pykube.CustomResourceDefinition.objects(api).all() ]
   settings.persistence.diffbase_storage = kopf.StatusDiffBaseStorage(field='status.diff-base')
 
 @kopf.index('apps', 'v1', 'deployment')
-async def deployments_idx(name, namespace, logger, **kwargs):
-  logger.info('index deployments')
+async def deployments_idx(name, namespace, body, **kwargs):
   _update_index_map(**kwargs)
-  return {namespace: name}
-  # result in: (see https://kopf.readthedocs.io/en/stable/indexing/#index-content)
-  # {'namespace1': ['nameA', 'nameB'],
-  #  'namespace2': ['nameC']}
+  ## NOTE: see https://kopf.readthedocs.io/en/stable/indexing/#index-content
+  return {f'{namespace}:{name}': body}
+
+@kopf.index('apps', 'v1', 'statefulset')
+async def statefulsets_idx(name, namespace, body, **kwargs):
+  _update_index_map(**kwargs)
+  ## NOTE: see https://kopf.readthedocs.io/en/stable/indexing/#index-content
+  return {f'{namespace}:{name}': body}
+
+@kopf.index('apps', 'v1', 'daemonset')
+async def daemonsets_idx(name, namespace, body, **kwargs):
+  _update_index_map(**kwargs)
+  ## NOTE: see https://kopf.readthedocs.io/en/stable/indexing/#index-content
+  return {f'{namespace}:{name}': body}
 
 @kopf.index('', 'v1', 'pod')
-async def pods_idx(name, namespace, logger, **kwargs):
-  logger.info('index pods')
+async def pods_idx(name, namespace, body, **kwargs):
   _update_index_map(**kwargs)
-  return {namespace: name}
+  return {f'{namespace}:{name}': body}
 
 
 async def start(shutdown_event: asyncio.Event):
--- k8s_agent/webapp.py
+++ k8s_agent/webapp.py
@@ -1,31 +1,28 @@
 
 import asyncio
 
-from fastapi import FastAPI, APIRouter, Request, Response
+from fastapi import FastAPI, Request
 
 from hypercorn.asyncio import serve
 from hypercorn.config import Config
 
 from . import settings, k8sController
 
-from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 app = FastAPI()
 
 @app.get("/")
 async def root(request: Request):
-  content = {}
+  content = {'crds': k8sController.CRDS }
   for index_name in k8sController.index_list():
     idx = k8sController.get_index(index_name=index_name)
-    content['index_name'] = {
-      namespace: list(idx[namespace])
-      for namespace in idx.keys()
-    }
+    content[index_name.replace('_idx', '')] = list(idx.keys())
+
   return JSONResponse(content=content)
 
 
-async def start(shutdown_event: asyncio.Event):
 
+async def start(shutdown_event: asyncio.Event):
   config = Config()
   config.bind = [f'0.0.0.0:{settings.PORT}']
   config.use_reloader = settings.DEBUG
